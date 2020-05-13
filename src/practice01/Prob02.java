6062f6d907e5f6b3c6f212fcf43318d16fbc317f
bit <bit@DESKTOP-DJMLAPN>
2020-05-13 21:55:09 +0900

    undone02

--- src/practice01/Prob02.java
+++ src/practice01/Prob02.java
@@ -4,10 +4,10 @@ public class Prob02 {
 
 	public static void main(String[] args) {
 
-		for(int i =0; i<9; i++) {
+		for(int i= 0; i<9; i++) {
 			
-			for(int j=1; j <i+9; j++) {				
-			System.out.print(i + j);
+			for(int j=i+1; j <i+11; j++) {				
+			System.out.print(j);
 		
 			}	
 			
--- src/practice01/Prob03.java
+++ src/practice01/Prob03.java
@@ -7,6 +7,29 @@ public class Prob03 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
+		/*
+		 * a. 입력 받은 숫작 홀수/인 경우, /0부터 입력 값까지 /홀수의 합 출력 
+		 * 
+		 * b. 입력 받은 숫자가 짝수 인 경우, 0부터 입력 값까지 짝수의 합 출력
+		 */
+		int a = scanner.nextInt();
+	//	int b = scanner.nextInt();
+		
+		if(a%2 == 1) {
+
+			    int sum = 0;
+				for(int result = 0; result <= a; result++ )
+				{
+					if(result%2 == 0) {
+						continue;
+					}
+					sum = sum + result;
+					System.out.println(sum);				
+			}
+				
+			
+		  
+		}
 
 		scanner.close();
 		
