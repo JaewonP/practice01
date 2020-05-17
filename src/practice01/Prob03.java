0fc6ff96851a58ae2766d51c0b6bdf0108584b30
Park <Park@DESKTOP-GJBLH82>
2020-05-17 17:58:02 +0900

    homework done

--- src/practice01/Prob03.java
+++ src/practice01/Prob03.java
@@ -13,23 +13,40 @@ public class Prob03 {
 		 * b. 입력 받은 숫자가 짝수 인 경우, 0부터 입력 값까지 짝수의 합 출력
 		 */
 		int a = scanner.nextInt();
-	//	int b = scanner.nextInt();
+		//int b = scanner.nextInt();
 		
+		// a의 경우
 		if(a%2 == 1) {
 
-			    int sum = 0;
+			    int sum1 = 0;
 				for(int result = 0; result <= a; result++ )
 				{
 					if(result%2 == 0) {
 						continue;
 					}
-					sum = sum + result;
-					System.out.println(sum);				
+					sum1 = sum1 + result;
+								
 			}
 				
-			
+				System.out.println(sum1);	
 		  
+		}	
+		//b의 경우 
+		else if(a%2 == 0) {
+
+		    int sum2 = 0;
+			for(int result = 0; result <= a; result++ )
+			{
+				if(result%2 == 1) {
+					continue;
+				}
+				sum2 = sum2 + result;
+							
 		}
+			
+			System.out.println(sum2);	
+	  
+	}		
 
 		scanner.close();
 		
--- src/practice01/Prob05.java
+++ src/practice01/Prob05.java
@@ -4,25 +4,28 @@ public class Prob05 {
 
 	public static void main(String[] args) {
 		// 3, 6, 9 
-		int n = 36;
-		String s = String.valueOf(n); // 숫자를 문자로 만듬 
-		
-		System.out.println(s);
-		
-		int clapCount = 0;
-		int length = s.length();
-		for(int i=0; i < length; i++) {
-			char c = s.charAt(i);
-			if(c == '3' || c == '6' || c == '9') {
-				clapCount++;
-			}
-		}// int count -> 결과 값에 따라 짝 
-		
-		
-		System.out.println(clapCount);
-		
-		for( int j = 1; j <= clapCount; j++ ) {
-			System.out.println("짝");
+		//int n = 36;
+		for(int raise_num = 1; raise_num <= 100; raise_num++)
+		{
+			
+			String s = String.valueOf(raise_num); // 숫자를 문자로 만듬 
+			
+			int clapCount = 0;
+			int length = s.length();
+			for(int i=0; i < length; i++) {
+				char c = s.charAt(i);
+				if(c == '3' || c == '6' || c == '9') {
+					clapCount++;
+				}
+			}// int count -> 결과 값에 따라 짝 
+			
+			
+			for( int j = 1; j <= clapCount; j++ ) {
+				System.out.println(s + "짝");	
+			
 		}
+			// System.out.println(clapCount);
+			// System.out.println(s);
+}
 	}
 }
