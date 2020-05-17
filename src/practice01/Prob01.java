c4b439bd59b99af58c96bf2374d80800bec89adc
Park <Park@DESKTOP-GJBLH82>
2020-05-17 18:00:07 +0900

    homework done

--- src/practice01/Prob01.java
+++ src/practice01/Prob01.java
@@ -6,7 +6,7 @@ public class Prob01 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
-		
+		//
 		System.out.print( "수를 입력하세요 : " );
 		int number = scanner.nextInt();
 
--- src/practice01/Prob02.java
+++ src/practice01/Prob02.java
@@ -4,11 +4,10 @@ public class Prob02 {
 
 	public static void main(String[] args) {
 
+		//
 		for(int i= 0; i<9; i++) {
-			
 			for(int j=i+1; j <i+11; j++) {				
 			System.out.print(j);
-		
 			}	
 			
 		System.out.print("\n");
